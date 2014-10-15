23bbec1dc8b07bc7e2a766c1d48b7bee1c692864
porcupo <git@porcupo.net>
2014-10-15 17:56:40 -0400

    initial

---
+++ spec/localhost/httpd_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe service('nginx') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe file('/etc/nginx/sites-enabled/porcupo') do
+  it { should be_file }
+  its(:content) { should match /server_name porcupo.net \*\.porcupo.net localhost/ }
+end
+
+describe host('porcupo.net') do
+  it { should be_resolvable }
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'serverspec'
+require 'pathname'
+
+include SpecInfra::Helper::Exec
+include SpecInfra::Helper::DetectOS
+
+RSpec.configure do |c|
+  if ENV['ASK_SUDO_PASSWORD']
+    require 'highline/import'
+    c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
+  else
+    c.sudo_password = ENV['SUDO_PASSWORD']
+  end
+  c.before :all do
+    c.os = backend(Serverspec::Commands::Base).check_os
+  end
+end
