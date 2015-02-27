b8ae68e241658404832565610950b86abf1917d4
porcupo <github@porcupo.net>
2015-02-26 21:47:20 -0800

    cleanup helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,28 +2,12 @@ require 'serverspec'
 require 'net/ssh'
 require 'highline/import'
 
-set :backend, :ssh
-
-if ENV['SUDO_PASSWORD']
-  set :sudo_password, ENV['SUDO_PASSWORD']
-else
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-end
-
 host = ENV['TARGET_HOST']
-
 options = Net::SSH::Config.for(host)
-
 options[:user] ||= Etc.getlogin
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
-
-# this should probably be set on an as-needed basis
+set :backend, :ssh
 set :disable_sudo, true
-
-# Set environment variables
 set :env, :LANG => 'en_US.UTF-8'
-
-# Set PATH
-# set :path, '/sbin:/usr/local/sbin:$PATH'
